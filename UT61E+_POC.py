18e9d99adc4177db0e7581c887431fd091dc516e
jalzat-e <68554060+jalzat-e@users.noreply.github.com>
2024-11-04 16:58:43 -0500

    Update UT61E+_POC.py
    Suggested changes to run with python3-hid 0.9.0, because the original code generates:
    TypeError: 'hid.device' object does not support the context manager protocol (HIDAPI perhaps now lacks a context manager?)
    The changes also fit the hidapi page examples (https://trezor.github.io/cython-hidapi/examples.html)

--- UT61E+_POC.py
+++ UT61E+_POC.py
@@ -2,11 +2,15 @@ import hid as hid
 import time
 from ctypes import *
 
-with hid.Device(0x10C4, 0xEA80) as h:
+# Commented out because error TypeError: 'hid.device' object does not support the context manager protocol
+#with hid.device() as h:
+try:
+	h=hid.device()
+	h.open(0x10C4, 0xEA80)
 
-    acu = []
+	acu = []
 
-    RANGE_RESISTANCE = [
+	RANGE_RESISTANCE = [
         "Ω", #220.00Ω
         "KΩ", #2.2000KΩ
         "KΩ", #22.000KΩ
@@ -16,7 +20,7 @@ with hid.Device(0x10C4, 0xEA80) as h:
         "MΩ"  #220.00MΩ
     ]
 
-    RANGE_FREQUENCY = [
+	RANGE_FREQUENCY = [
         "Hz", #22.00Hz
         "Hz", #220.0Hz
         "kHz", #22.000KHz
@@ -26,7 +30,7 @@ with hid.Device(0x10C4, 0xEA80) as h:
         "MHz"  #220.00MHz
     ]
 
-    RANGE_CAPACITANCE = [
+	RANGE_CAPACITANCE = [
         "nF", #22.000nF
         "nF", #220.00nF
         "µF", #2.2000μF
@@ -37,111 +41,113 @@ with hid.Device(0x10C4, 0xEA80) as h:
         "mF"  #220.00mF
     ]
 
-    def write_config(data):
-        """write configuration with send_feature_report"""
-        len_bytes = len(data)
-        buf = create_string_buffer(len_bytes)
-        buf[:len_bytes] = data[:len_bytes]
-        h.send_feature_report(buf)
+	def write_config(data):
+		"""write configuration with send_feature_report"""
+		len_bytes = len(data)
+		buf = create_string_buffer(len_bytes)
+		buf[:len_bytes] = data[:len_bytes]
+		h.send_feature_report(buf)
 
-    def write(data):
-        len_bytes = len(data)
-        buf = create_string_buffer(len_bytes + 1)
-        # Store the length of data in buf[0]
-        buf[0] = len_bytes
-        buf[1:] = data[:len_bytes]
-        h.write(buf)
+	def write(data):
+		len_bytes = len(data)
+		buf = create_string_buffer(len_bytes + 1)
+		# Store the length of data in buf[0]
+		buf[0] = len_bytes
+		buf[1:] = data[:len_bytes]
+		h.write(buf)
 
-    def read_raw():
-        counter = 0
-        while counter < 10:
-            counter += 1
-            time.sleep(0.10)
-            b = h.read(2, 1)
-            if len(b) > 0:
-                acu.append(b[1])
-                counter = 0
+	def read_raw():
+		counter = 0
+		while counter < 10:
+			counter += 1
+			time.sleep(0.10)
+			b = h.read(2, 1)
+			if len(b) > 0:
+				acu.append(b[1])
+				counter = 0
 
-    def parse_func():
-        if (acu[3] == 0x00): return "AC Voltage"
-        elif (acu[3] == 0x01): return "AC Voltage"
-        elif (acu[3] == 0x02): return "DC Voltage"
-        elif (acu[3] == 0x03): return "DC Voltage"
-        elif (acu[3] == 0x04): return "Frequency"
-        elif (acu[3] == 0x05): return "Duty"
-        elif (acu[3] == 0x06): return "Resistance"
-        elif (acu[3] == 0x07): return "Continuity"
-        elif (acu[3] == 0x08): return "Diodes"
-        elif (acu[3] == 0x09): return "Capacitance"
-        elif (acu[3] == 0x0C): return "DC Current"
-        elif (acu[3] == 0x0D): return "AC Current"
-        elif (acu[3] == 0x0E): return "DC Current"
-        elif (acu[3] == 0x0F): return "AC Current"
-        elif (acu[3] == 0x10): return "DC Current"
-        elif (acu[3] == 0x11): return "AC Current"
-        elif (acu[3] == 0x12): return "hFE"
-        elif (acu[3] == 0x14): return "NCV"
-        elif (acu[3] == 0x18): return "AC Voltage LPF"
-        elif (acu[3] == 0x19): return "DC+AC"
+	def parse_func():
+		if (acu[3] == 0x00): return "AC Voltage"
+		elif (acu[3] == 0x01): return "AC Voltage"
+		elif (acu[3] == 0x02): return "DC Voltage"
+		elif (acu[3] == 0x03): return "DC Voltage"
+		elif (acu[3] == 0x04): return "Frequency"
+		elif (acu[3] == 0x05): return "Duty"
+		elif (acu[3] == 0x06): return "Resistance"
+		elif (acu[3] == 0x07): return "Continuity"
+		elif (acu[3] == 0x08): return "Diodes"
+		elif (acu[3] == 0x09): return "Capacitance"
+		elif (acu[3] == 0x0C): return "DC Current"
+		elif (acu[3] == 0x0D): return "AC Current"
+		elif (acu[3] == 0x0E): return "DC Current"
+		elif (acu[3] == 0x0F): return "AC Current"
+		elif (acu[3] == 0x10): return "DC Current"
+		elif (acu[3] == 0x11): return "AC Current"
+		elif (acu[3] == 0x12): return "hFE"
+		elif (acu[3] == 0x14): return "NCV"
+		elif (acu[3] == 0x18): return "AC Voltage LPF"
+		elif (acu[3] == 0x19): return "DC+AC"
 
-    def parse_unit():
+	def parse_unit():
 
-        if ((acu[3] == 0x00) | (acu[3] == 0x02) | (acu[3] == 0x08) |
+		if ((acu[3] == 0x00) | (acu[3] == 0x02) | (acu[3] == 0x08) |
             (acu[3] == 0x18) | (acu[3] == 0x19)): return "V"
-        elif ((acu[3] == 0x01) | (acu[3] == 0x03)): return "mV"
-        elif (acu[3] == 0x04): return RANGE_FREQUENCY[acu[4]&0xF]
-        elif (acu[3] == 0x05): return "%"
-        elif (acu[3] == 0x06): return RANGE_RESISTANCE[acu[4]&0xF]
-        elif (acu[3] == 0x07): return "Ω"
-        elif (acu[3] == 0x09): return RANGE_CAPACITANCE[acu[4]&0xF]
-        elif ((acu[3] == 0x0C) | (acu[3] == 0x0D)): return "μA"
-        elif ((acu[3] == 0x0E) | (acu[3] == 0x0F)): return "mA"
-        elif ((acu[3] == 0x10) | (acu[3] == 0x11)): return "A"
-        elif (acu[3] == 0x12): return "β"
+		elif ((acu[3] == 0x01) | (acu[3] == 0x03)): return "mV"
+		elif (acu[3] == 0x04): return RANGE_FREQUENCY[acu[4]&0xF]
+		elif (acu[3] == 0x05): return "%"
+		elif (acu[3] == 0x06): return RANGE_RESISTANCE[acu[4]&0xF]
+		elif (acu[3] == 0x07): return "Ω"
+		elif (acu[3] == 0x09): return RANGE_CAPACITANCE[acu[4]&0xF]
+		elif ((acu[3] == 0x0C) | (acu[3] == 0x0D)): return "μA"
+		elif ((acu[3] == 0x0E) | (acu[3] == 0x0F)): return "mA"
+		elif ((acu[3] == 0x10) | (acu[3] == 0x11)): return "A"
+		elif (acu[3] == 0x12): return "β"
 
-        return ""
+		return ""
 
-    def check_crc():
-        crc_calc = sum(acu[:17])
-        crc_dmm = acu[17] << 8 | acu[18]
-        if (crc_calc == crc_dmm):
-            return "OK"
-        return "KO (got 0x{:X}".format(crc_calc) + ", should've been 0x{:X})".format(crc_dmm)
+	def check_crc():
+		crc_calc = sum(acu[:17])
+		crc_dmm = acu[17] << 8 | acu[18]
+		if (crc_calc == crc_dmm):
+			return "OK"
+		return "KO (got 0x{:X}".format(crc_calc) + ", should've been 0x{:X})".format(crc_dmm)
 
-    def parse_flags():
-        ret = ""
-        if (acu[14] & 0x1): ret += "REL, "
-        if (acu[14] & 0x2): ret += "HLD, "
-        if (acu[14] & 0x4): ret += "MIN, "
-        if (acu[14] & 0x8): ret += "MAX, "
+	def parse_flags():
+		ret = ""
+		if (acu[14] & 0x1): ret += "REL, "
+		if (acu[14] & 0x2): ret += "HLD, "
+		if (acu[14] & 0x4): ret += "MIN, "
+		if (acu[14] & 0x8): ret += "MAX, "
 
-        if (acu[15] & 0x4): ret += "MAN, "
+		if (acu[15] & 0x4): ret += "MAN, "
 
-        if (acu[16] & 0x2): ret += "PMIN, "
-        if (acu[16] & 0x4): ret += "PMAX, "
-        if (acu[16] & 0x8): ret += "UKW, " # Unknow
+		if (acu[16] & 0x2): ret += "PMIN, "
+		if (acu[16] & 0x4): ret += "PMAX, "
+		if (acu[16] & 0x8): ret += "UKW, " # Unknow
 
-        return ret[:-2]
+		return ret[:-2]
 
-    h.get_feature_report(0x46, 64)
+	h.get_feature_report(0x46, 64)
 
-    write_config([0x41, 0x01])
-    write_config([0x50, 0x00, 0x00, 0x25, 0x80, 0x00, 0x00, 0x03, 0x00])
-    write_config([0x43, 0x03])
+	write_config([0x41, 0x01])
+	write_config([0x50, 0x00, 0x00, 0x25, 0x80, 0x00, 0x00, 0x03, 0x00])
+	write_config([0x43, 0x03])
 
-    write([ 0xab, 0xcd, 0x03, 0x5e, 0x01, 0xd9 ])
-    read_raw()
-    print("Maker ID: 0x" + ''.join('%02X'%i for i in acu[:2]))
-    print("Response Lenght: 0x" + ''.join('%02X'%i for i in acu[2:3]))
-    print("Selected Function: " + parse_func())
-    print("Unit: " + parse_unit())
-    print("Value: " + ''.join('%c'%c for c in acu[5:12]))
-    print("Analog Bar:", end = "")
-    if (acu[16] & 0x1): print("-", end = "")
-    print("|" + "---------|" * acu[12] + "-" * acu[13])
-    print("Flags: " + parse_flags())
-    print("CRC: " + check_crc())
-
+	write([ 0xab, 0xcd, 0x03, 0x5e, 0x01, 0xd9 ])
+	read_raw()
+	print("Maker ID: 0x" + ''.join('%02X'%i for i in acu[:2]))
+	print("Response Lenght: 0x" + ''.join('%02X'%i for i in acu[2:3]))
+	print("Selected Function: " + parse_func())
+	print("Unit: " + parse_unit())
+	print("Value: " + ''.join('%c'%c for c in acu[5:12]))
+	print("Analog Bar:", end = "")
+	if (acu[16] & 0x1): print("-", end = "")
+	print("|" + "---------|" * acu[12] + "-" * acu[13])
+	print("Flags: " + parse_flags())
+	print("CRC: " + check_crc())
+	
+except IOError as ex:
+	print(ex)
 ##    print("Testing Select CMD")
 ##    time.sleep(2)
 ##    write([ 0xab, 0xcd, 0x03, 0x4c, 0x01, 0xc7 ])
